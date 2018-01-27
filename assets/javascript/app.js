fd66fdc771cc1795fb2bbd5f0979c9801bf46fee
ehler orngard <ehlerorngard@ehlers-MacBook.local>
2018-01-27 02:16:39 -0800

    all but click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,44 +8,29 @@ var keySet = [Questions.q1, Questions.q2, Questions.q3];
 
 var totalRightAnswers = 0;
 var whatQuestion = 0;
-// var thePresentQuestion;
 var timer = 10;
 var theCountdown;
-var answeredCorrectly;
 var randomA;
 
 function showNewQuestion() {
-	// answeredCorrectly = false;
-
 	$("#questionText").text(keySet[whatQuestion][0]);
-
+	$("#optionButtons").empty();
 	var answerArray = [1, 2, 3];
 	var rIndex;
 	var choice;
 	for (i = 0; i < keySet.length; i++) {
 		rIndex = Math.floor(Math.random()*answerArray.length);
-		console.log(rIndex);
 		randomA = answerArray[rIndex];
-		console.log(randomA);
 		var btn = $("<button>");
 		btn.addClass("buttons");
-		btn.attr("data-answer", keySet[whatQuestion][randomA]);
 		choice = keySet[whatQuestion][randomA];
-		console.log(keySet[whatQuestion][3]);
+		btn.attr("data-answer", randomA);
 		btn.text(choice);
 		answerArray.splice(rIndex, 1);
 		$("#optionButtons").append(btn);
 	}
-	timer = 10;
+	timer = 2;
 	theCountdown = setInterval(decrement, 1000);
-
-
-	// if () // answers correctly..
-		// clearInterval(theCountdown);
-	// else if () //answers incorrectly...
-// 
-		// else if (timer === 0)
-
 }
 
 function decrement() {
@@ -53,28 +38,53 @@ function decrement() {
 	$("#timerReadout").text(timer);
 	if (timer === 0) {
 		clearInterval(theCountdown);
-		loadAnswerWrongScreen;
+		loadTimeUpScreen();
 	}
 }
 
 function loadAnswerRightScreen() {
+	totalRightAnswers++;
+	$("#questionText").text("That's CORRECT!");
+	$("#optionButtons").empty();
+	$("#optionButtons").text(" ––– nicely done ;)");
 	whatQuestion++;
-	setTimeout(showNewQuestion, 2000);
-	if (count === images.length) {
-    		count = 0;
-  	}
+	if (whatQuestion < keySet.length) {
+		setTimeout(showNewQuestion, 3000);
+	}
+	else if (whatQuestion === ketSet.length) {
+		showEndGame();
+	}
 }
 
 function loadAnswerWrongScreen() {
-
-}
+	$("#questionText").text("Sorry, that's incorrect...");
+	$("#optionButtons").empty();
+	$("#optionButtons").text("The answer was " + keySet[whatQuestion][1] + ".");
+	whatQuestion++;
+	if (whatQuestion < keySet.length) {
+		setTimeout(showNewQuestion, 3000);
+	}
+	else if (whatQuestion === ketSet.length) {
+		showEndGame();
+	}}
 
 function loadTimeUpScreen() {
-
+	$("#questionText").text("TIME'S UP!");
+	$("#optionButtons").empty();
+	$("#optionButtons").text("The answer was " + keySet[whatQuestion][1] + ".");
+	whatQuestion++;
+	if (whatQuestion < keySet.length) {
+		setTimeout(showNewQuestion, 3000);
+	}
+	else if (whatQuestion === keySet.length) {
+		setTimeout(showEndGame, 3000);
+	}
 }
 
 function showEndGame() {
-
+	$("#questionText").text("Well, that's the game!");
+	$("#optionButtons").empty();
+	$("#optionButtons").text("You answered " + totalRightAnswers + " out of " + keySet.length + " questions correctly.");
 }
 
 function restartGame() {
@@ -88,4 +98,11 @@ $(document).ready(function(){
 
 // document.on("click", lsdkf jlsdkjf)
 	// if (text == data-answer)
+
+	// if () // answers correctly..
+		// clearInterval(theCountdown);
+		// loadAnswerRightScreen();
+	// else if () //answers incorrectly...
+		// clearInterval(theCountdown);
+		// loadAnswerWrongScreen();
 
